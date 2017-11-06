be377801458395349b0a7b622b88c99a8cc96fbc
Juan Ramirez <ep.juan@gmail.com>
2017-11-06 11:00:46 -0500

    Primera Subida

--- RetornarValoresSQLDataReader/Form1.vb
+++ RetornarValoresSQLDataReader/Form1.vb
@@ -1,3 +1,32 @@
-﻿Public Class Form1
+﻿Imports System.Data.SqlClient
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Creacion de la Cadena de Conexion
+        Dim builder As New SqlConnectionStringBuilder
+        builder("Data Source") = "SERVIDOR01\SQLEXPRESS"
+        builder("Integrated Security") = True
+        builder("Initial Catalog") = "AdventureWorks2012"
+
+        ' Obtener mas de un valor de una tabla
+
+        Using connection As New SqlConnection(builder.ConnectionString)
+
+            Dim command As SqlCommand = New SqlCommand("Select DepartmentID, Name From HumanResources.Department", connection)
+            connection.Open()
+
+            Dim reader As SqlDataReader = command.ExecuteReader()
+
+            If reader.HasRows Then
+                Do While reader.Read()
+                    MsgBox(reader.GetValue(0) & vbTab & reader.GetString(1))
+                Loop
+
+            End If
+            reader.Close()
+
+
+
+        End Using
+    End Sub
 
 End Class
